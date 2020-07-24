b4e52c189ab5cb5da954df4ea6a277ff3fbcbc3f
Foussy <christian.hiricoiu@gmail.com>
2020-07-24 09:13:30 +0200

    Update FEA_Post_Process_Viewer.py

--- FEA_Post_Process_Viewer.py
+++ FEA_Post_Process_Viewer.py
@@ -2,8 +2,6 @@
 
 __author__ = "Christian Hiricoiu"
 __email__ = "christian.hiricoiu@gmail.com"
-__licence__ = "GPL"
-__version__ = "1.0"
 
 """
 Programme de visualisation d'analyse de calcul éléments finis.
