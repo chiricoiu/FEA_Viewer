c4b131a604e1b4980f512ae13e30c5be2e55b093
Foussy <christian.hiricoiu@gmail.com>
2020-07-24 09:12:53 +0200

    Update Delta_FEA_Database.py

--- Delta_FEA_Database.py
+++ Delta_FEA_Database.py
@@ -2,8 +2,6 @@
 
 __author__ = "Christian Hiricoiu"
 __email__ = "christian.hiricoiu@gmail.com"
-__licence__ = "GPL"
-__version__ = "1.0"
 
 """
 Ce programme récupère en entrée 2 database créées par le programme '02_FEA-Post-Processing-Database-Generator':
